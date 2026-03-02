1dd6cb9dcd4d5566710e0c86e995b4dd8e3e9b3c
Patrick Yin <patyin@cs.washington.edu>
2026-03-02 10:03:25 -0800

    nit fix

--- diffusion_policy/workspace/train_mlp_image_workspace.py
+++ diffusion_policy/workspace/train_mlp_image_workspace.py
@@ -225,6 +225,7 @@ class TrainMLPImageWorkspace(BaseWorkspace):
                                 os.makedirs(os.path.dirname(step_ckpt_path), exist_ok=True)
                                 self.save_checkpoint(path=step_ckpt_path)
                                 self.model = model_ddp
+                                self.model.train()
 
                                 # Update last checkpoint step
                                 self.last_checkpoint_step = self.global_step
@@ -283,6 +284,8 @@ class TrainMLPImageWorkspace(BaseWorkspace):
                         del result
                         del pred_action
                         del mse
+
+                policy.train()
 
                 if self.accelerator.is_main_process:
                     wandb_run.log(step_log, step=self.global_step)
